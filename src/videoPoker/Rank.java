5bf2100a2cfc5529cf48d38c5e7109c5b4be7a20
LUNKUS <sigitas@lunkus.com>
2019-06-27 17:47:44 +0300

    enum Rank Class is used for constants in program

---
+++ src/videoPoker/Rank.java
@@ -0,0 +1,15 @@
+package videoPoker;
+
+/**
+ *  enum Rank Class it's a special datatype that is used
+ *  for constants in program
+ *  Ranks are always gonna be only:
+ *  ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+ *  we can use an enum because those are going to be constant
+ */
+
+public enum Rank {
+
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+
+}

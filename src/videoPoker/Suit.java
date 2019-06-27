946e22d18a678419b7bca78b019799c5b86a717b
LUNKUS <sigitas@lunkus.com>
2019-06-27 17:47:26 +0300

    enum Suit Class is used for constants in program

---
+++ src/videoPoker/Suit.java
@@ -0,0 +1,13 @@
+package videoPoker;
+
+/**
+ *  enum Suit Class it's a special datatype that is used
+ *  for constants in program
+ *  Cards are always gonna be only: CLUBS, DIAMONDS, SPADES & HEARTS
+ *  we can use an enum because those are going to be constant
+ */
+
+public enum Suit {
+
+    CLUBS, DIAMONDS, SPADES, HEARTS
+}
